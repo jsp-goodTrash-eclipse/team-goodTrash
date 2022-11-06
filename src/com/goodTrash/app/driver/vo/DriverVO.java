c7c803a9939d849046b65573aef7978a6e7dd33c
caMelodian <intelliJHLee@gmail.com>
2022-11-06 20:53:42 +0900

    "기사 회원가입 1차 작업"

--- src/com/goodTrash/app/driver/vo/DriverVO.java
+++ src/com/goodTrash/app/driver/vo/DriverVO.java
@@ -1,5 +1,93 @@
 package com.goodTrash.app.driver.vo;
 
 public class DriverVO {
+	private String driverId;
+	private int driverNumber;
+	private String driverPassword;
+	private String driverPhoneNumber;
+	private String driverName;
+	private String driverEmail;
+	private int driverStatus;
+	private String driverAccountNumber;
+	private String driverBank;
+	private String driverArea;
+	
+	
+	
+	public DriverVO() {;}
+	
+	public String getDriverId() {
+		return driverId;
+	}
+	public void setDriverId(String driverId) {
+		this.driverId = driverId;
+	}
+	public int getDriverNumber() {
+		return driverNumber;
+	}
+	public void setDriverNumber(int driverNumber) {
+		this.driverNumber = driverNumber;
+	}
+	public String getDriverPassword() {
+		return driverPassword;
+	}
+	public void setDriverPassword(String driverPassword) {
+		this.driverPassword = driverPassword;
+	}
+	public String getDriverPhoneNumber() {
+		return driverPhoneNumber;
+	}
+	public void setDriverPhoneNumber(String driverPhoneNumber) {
+		this.driverPhoneNumber = driverPhoneNumber;
+	}
+	public String getDriverName() {
+		return driverName;
+	}
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+	public String getDriverEmail() {
+		return driverEmail;
+	}
+	public void setDriverEmail(String driverEmail) {
+		this.driverEmail = driverEmail;
+	}
+	public int getDriverStatus() {
+		return driverStatus;
+	}
+	public void setDriverStatus(int driverStatus) {
+		this.driverStatus = driverStatus;
+	}
+	public String getDriverAccountNumber() {
+		return driverAccountNumber;
+	}
+	public void setDriverAccountNumber(String driverAccountNumber) {
+		this.driverAccountNumber = driverAccountNumber;
+	}
+	public String getDriverBank() {
+		return driverBank;
+	}
+	public void setDriverBank(String driverBank) {
+		this.driverBank = driverBank;
+	}
+	public String getDriverArea() {
+		return driverArea;
+	}
+	public void setDriverArea(String driverArea) {
+		this.driverArea = driverArea;
+	}
+	@Override
+	public String toString() {
+		return "DriverVO [driverId=" + driverId + ", driverNumber=" + driverNumber + ", driverPassword="
+				+ driverPassword + ", driverPhoneNumber=" + driverPhoneNumber + ", driverName=" + driverName
+				+ ", driverEmail=" + driverEmail + ", driverStatus=" + driverStatus + ", driverAccountNumber="
+				+ driverAccountNumber + ", driverBank=" + driverBank + ", driverArea=" + driverArea + "]";
+	}
+	
+	
+	
+	
+	
+	
 	
 }
